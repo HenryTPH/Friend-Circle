8823aa26ddb85770322d4a52dd7541c71fca7982
Henry Tu <hungtp2912@gmail.com>
2023-10-01 12:48:31 -0600

    Create the connection string, add migrations

--- DatingApp/API/Controllers/WeatherForecastController.cs
+++ DatingApp/API/Controllers/WeatherForecastController.cs
@@ -11,12 +11,12 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
+    // private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
+    // public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    // {
+    //     _logger = logger;
+    // }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
