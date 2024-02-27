cc893a289f70cbed1390077ad33049a056cf1683
Henry Tu <hungtp2912@gmail.com>
2024-02-26 23:33:23 -0600

    Adding a photo uploader

---
+++ DatingApp/client/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+
+@Component({
+  selector: 'app-photo-editor',
+  templateUrl: './photo-editor.component.html',
+  styleUrls: ['./photo-editor.component.css']
+})
+export class PhotoEditorComponent implements OnInit {
+  @Input() member: Member | undefined;
+
+  constructor(){
+    
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
